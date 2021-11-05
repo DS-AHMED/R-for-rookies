dd58a196353ed2fca084447caa331982f4007d30
DS-AHMED <85762315+DS-AHMED@users.noreply.github.com>
2021-11-05 23:53:22 +0300

    Add files via upload

---
+++ R Introduction.R
@@ -0,0 +1,10 @@
+
+
+#types of classes in R
+
+x <- c(0.5 , 0.6) # numeric
+x <- c(TRUE , FALSE ) # logical
+x <- c(T, F) # logical
+x <- c("a", "b", "c") # character
+x <- 9:29 # integer
+x <- c (1+0i, 2+4i) # complex
